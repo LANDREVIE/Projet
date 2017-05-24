9ddfea2f12bd4cb2441ce59f2348564f47b7d3fd
Fred <frederik_2@hotmail.fr>
2017-05-24 16:51:46 +0200

    gps

--- app/src/main/java/com/example/josel/projet/PostActivity.java
+++ app/src/main/java/com/example/josel/projet/PostActivity.java
@@ -1,11 +1,16 @@
 package com.example.josel.projet;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,6 +27,9 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 public class PostActivity extends AppCompatActivity {
 
@@ -69,6 +77,8 @@ public class PostActivity extends AppCompatActivity {
             }
         });
 
+
+
         mSubmitBtn.setOnClickListener( new View.OnClickListener(){
             @Override
             public void onClick(View view){
@@ -76,15 +86,69 @@ public class PostActivity extends AppCompatActivity {
 
 
             }
+
+
+
+
         });}
 
+
+
+    @SuppressWarnings("MissingPermission")
     private void startPosting(){
+
+
 
         mProgress.setMessage("Posting to Blog ...");
         mProgress.show();
         final String title_val = mPostTitle.getText().toString().trim();
         final String desc_val = mPostDesc.getText().toString().trim();
 
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd_HH:mm:ss");
+        final String currentDateandTime = sdf.format(new Date());
+
+        LocationManager locManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                if (location != null) {
+                    Log.e("Latitude :", "" + location.getLatitude());
+                    Log.e("Longitude :", "" + location.getLongitude());
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000,10, locationListener);
+        Location location = locManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+        double latitudeDouble=0;
+        double longitudeDouble=0;
+        latitudeDouble = location.getLatitude();
+        longitudeDouble = location.getLongitude();
+
+        String StrLatitude=  String.valueOf(latitudeDouble);
+        String StrLongitude = String.valueOf(longitudeDouble);
+        final String StrPosition = "Latitude: "+StrLatitude+", Longitude: "+StrLongitude;
+
+
+
+        // Debut du Tutoriel
         if(!TextUtils.isEmpty(title_val) && !TextUtils.isEmpty(desc_val) && mImageUri != null){
             StorageReference filepath = mStorage.child("Blog_Images").child(mImageUri.getLastPathSegment());
             filepath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
@@ -97,8 +161,8 @@ public class PostActivity extends AppCompatActivity {
 
                     newPost.child("title").setValue(title_val);
                     newPost.child("desc").setValue(desc_val);
-                    newPost.child("date").setValue("test");
-                    newPost.child("location").setValue("test");
+                    newPost.child("date").setValue(currentDateandTime);
+                    newPost.child("location").setValue(StrPosition);
                     newPost.child("image").setValue(downloadUrl.toString());
 
                     mProgress.dismiss();
